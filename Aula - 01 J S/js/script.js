ddf8db5e5f8b4ee509518c01fce79ca201adefe3
gilvanrb <gilvanrb.barros@gmail.com>
2025-02-26 20:36:39 -0300

    Java
    JavaScript

---
+++ Aula - 01 J S/js/script.js
@@ -0,0 +1,8 @@
+  document.getElementById("demo").innerHTML = "Estuda que a vida muda! Lute e lute novamente até cordeiros virarem leões."
+
+    function Aumentar(){
+    document.getElementById("demo").style.fontSize = "35px";
+  }
+  function Diminuir(){
+    document.getElementById("demo").style.fontSize = "15px";
+  }
