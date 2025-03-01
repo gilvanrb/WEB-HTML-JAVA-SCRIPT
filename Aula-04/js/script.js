9ee5201c560704a9de878b2ec1eb407e2faec9e1
gilvanrb <gilvanrb.barros@gmail.com>
2025-02-28 21:31:04 -0300

    Aula-04
    js.css

---
+++ Aula-04/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("btnMulher").addEventListener("click", function(){
+    document.getElementById("header").style.backgroundColor = "blueviolet"
+    document.getElementById("img").src = "img/mulher.avif";
+});
+
+document.getElementById("btnHomem").addEventListener("click", function(){
+    document.getElementById("header").style.backgroundColor = " red"
+    document.getElementById("img").src = "img/homem.jpg";
+});
